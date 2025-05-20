68939c27ead3d33fa8357fb81fa4f0ef4750d955
agopal45 <akhilgopal0824@gmail.com>
2025-05-20 14:50:29 -0400

    implements if-else logic to replace switch logic

--- src/javaCalculator.java
+++ src/javaCalculator.java
@@ -8,25 +8,20 @@ public class javaCalculator {
 
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
-
-        switch(operation) {
-            case ADD:
+        
+        if (operation == Operation.ADD) {
                 // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
+            result = num1 + num2;
+        } else if (operation == Operation.SUBTRACT) {
+            result = num1 - num2;
+        } else if (operation == Operation.MULTIPLY) {
+            result = num1 * num2;
+        } else if (operation == Operation.DIVIDE) {
+            if (num2 == 0) {
+                System.out.println("Can't divide by 0");
+            } else {
+                result = ((double)num1) / num2;
+            }
         }
         return result;
     }
