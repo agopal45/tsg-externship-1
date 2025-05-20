1506690e5b2d870a698e25757afe00152b771921
agopal45 <akhilgopal0824@gmail.com>
2025-05-20 15:05:27 -0400

    fixing quotation marks

--- src/javaCalculator.java
+++ src/javaCalculator.java
@@ -7,27 +7,27 @@ enum Operation
 public class javaCalculator {
 
     public static double basicCalculator(Operation operation, int num1, int num2){
-        double result = 0;
-        switch(operation) {
-            case ADD:
-                System.out.println(&quot;this workshop sucks&quot;);
-                break;
-            case SUBTRACT:
-                System.out.println(&quot;I hate working at this company&quot;);
-                break;
-            case MULTIPLY:
-                System.out.println(&quot;My boss is the worst&quot;);
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println(&quot;Error: u suck&quot;);
-                } else {
-                    result = num1 / num2;
-                }
-                break;
-        }
-        return result;
-    }
+        double result = 0;
+        switch(operation) {
+            case ADD:
+                System.out.println("this workshop sucks";);
+                break;
+            case SUBTRACT:
+                System.out.println("I hate working at this company");
+                break;
+            case MULTIPLY:
+                System.out.println("My boss is the worst");
+                break;
+            case DIVIDE:
+                if (num2 == 0) {
+                    System.out.println("Error: u suck");
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+        }
+        return result;
+    }
 
     public static void main(final String[] args) {
         System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
