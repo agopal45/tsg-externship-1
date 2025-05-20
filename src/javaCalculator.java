0b683f8360e430cc31668bd1b81cf3d6bb8d36d4
Akhil Gopal <152526455+agopal45@users.noreply.github.com>
2025-05-20 14:59:29 -0400

    Update javaCalculator.java

--- src/javaCalculator.java
+++ src/javaCalculator.java
@@ -6,30 +6,28 @@ enum Operation
 }
 public class javaCalculator {
 
-    public static double basicCalculator(final Operation operation, final int num1, final int num2){
-        double result = 0;
-
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
-        }
-        return result;
-    }
+    public static double basicCalculator(Operation operation, int num1, int num2){
+        double result = 0;
+        switch(operation) {
+            case ADD:
+                System.out.println(&quot;this workshop sucks&quot;);
+                break;
+            case SUBTRACT:
+                System.out.println(&quot;I hate working at this company&quot;);
+                break;
+            case MULTIPLY:
+                System.out.println(&quot;My boss is the worst&quot;);
+                break;
+            case DIVIDE:
+                if (num2 == 0) {
+                    System.out.println(&quot;Error: u suck&quot;);
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+        }
+        return result;
+    }
 
     public static void main(final String[] args) {
         System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
